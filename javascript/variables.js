d55585ff5530e5e0aa19029a48d1972006522750
skadima <sarahk3412@gmail.com>
2025-12-01 19:11:29 -0500

    Update variables.js

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,14 +1,15 @@
 // Using let
-let name = "Crystal";
-let age = 25;
+let name = "Sarah";
+let age = 22;
 let isStudent = true;
 
 document.getElementById("demo1").innerHTML =
   "Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
 
-var city = "Albany";     // Function-scoped
+var city = "Harlem";     // Function-scoped
 let state = "NY";        // Block-scoped
 const country = "Canada";   // Cannot be reassigned
 
 document.getElementById("demo2").innerHTML =
   "Location: " + city + ", " + state + ", " + country;
+
