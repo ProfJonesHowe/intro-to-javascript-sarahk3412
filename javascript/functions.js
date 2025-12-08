6498baab6135461195d0b57f6bd118301ebf71ad
skadima <sarahk3412@gmail.com>
2025-12-07 19:33:52 -0500

    Update functions.js

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,5 +1,13 @@
 function greetUser() {
   let name = document.getElementById("nameInput").value;
-  let message = "Hello, " + name + "! Welcome to JavaScript.";
+  let mood = document.getElementById("moodInput").value;
+
+  let message = "Hello, " + name + "! You are feeling " + mood + " today.";
   document.getElementById("greeting").innerHTML = message;
+}
+
+function clearFields() {
+  document.getElementById("nameInput").value = "";
+  document.getElementById("moodInput").value = "";
+  document.getElementById("greeting").innerHTML = "";
 }
