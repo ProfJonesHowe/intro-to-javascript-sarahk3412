29c2f9fd184763333d43676d1cd121c8850390c4
skadima <sarahk3412@gmail.com>
2025-12-07 19:40:00 -0500

    Update variables.js

--- javascript/variables.js
+++ javascript/variables.js
@@ -13,3 +13,18 @@ const country = "Canada";   // Cannot be reassigned
 document.getElementById("demo2").innerHTML =
   "Location: " + city + ", " + state + ", " + country;
 
+// Two new variables
+let a = 10;
+let b = 5;
+
+// One new function
+function runFinal() {
+  let total = a + b;
+
+  document.getElementById("finalResult").innerHTML =
+    "The total is: " + total;
+
+  console.log("Final Challenge Total:", total);
+}
+
+
